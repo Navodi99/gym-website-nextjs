91558ce328ca63716626f4eefd74ad6acb3ce165
Navodi99 <navodil99@gmail.com>
2026-03-19 15:00:23 +0530

    adding new styles for footer section

--- src/app/(components)/sections/footer/Footer.jsx
+++ src/app/(components)/sections/footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-between border-t border-(--primary-color) py-2 max-sm:gap-3 ">
+    <div className="flex items-center justify-between border-t border-(--primary-color) py-2 max-sm:gap-3 max-sm:flex-col">
       <div className="max-sm:hidden">
         <div className="rounded-full w-20 overflow-hidden ">
           <img src="/images/GymLogo.jpg" alt="Logo" className="w-40 "  />
@@ -11,13 +11,13 @@ const Footer = () => {
       </div>
 
       <div>
-        <p className="text-sm max-sm:text-xs ">© 2026  FITNESS CENTER. All rights reserved.</p>
+        <p className="text-sm">© 2026  FITNESS CENTER. All rights reserved.</p>
       </div>
       <div>
         <div className="text-(--primary-color) flex items-center justify-center gap-7">
-          <Facebook className="w-8 h-8 max-sm:w-4 max-sm:h-4"/>
-          <Instagram className="w-8 h-8 max-sm:w-4 max-sm:h-4"/>
-          <Linkedin className="w-8 h-8 max-sm:w-4 max-sm:h-4"/>
+          <Facebook className="w-8 h-8 max-sm:w-6 max-sm:h-6"/>
+          <Instagram className="w-8 h-8 max-sm:w-6 max-sm:h-6"/>
+          <Linkedin className="w-8 h-8 max-sm:w-6 max-sm:h-6"/>
         </div>
       </div>
     </div>
