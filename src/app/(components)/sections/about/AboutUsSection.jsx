b7d044ba1bb9f7dcb99c5e07f124f99a98036a24
Navodi99 <navodil99@gmail.com>
2026-03-19 15:19:28 +0530

    added responsive styles for about section

--- src/app/(components)/sections/about/AboutUsSection.jsx
+++ src/app/(components)/sections/about/AboutUsSection.jsx
@@ -31,15 +31,15 @@ const AboutUsSection = () => {
   ];
 
   return (
-    <div className="max-sm:hidden bg-(--gray-color) border-t-4 border-(--primary-color) rounded-3xl mb-15 p-5" id="about">
+    <div className="max-sm:flex-col bg-(--gray-color) border-t-4 border-(--primary-color) rounded-3xl mb-15 p-5 max-sm:my-2" id="about">
       <div>
-        <p className="uppercase tracking-widest font-bold text-2xl text-(--primary-color) text-center ">
+        <p className="uppercase tracking-widest font-bold text-2xl text-(--primary-color) text-center  max-sm:text-xl">
           About Us
         </p>
-        <p className="text-4xl text-center mt-5">
+        <p className="text-4xl text-center mt-5 max-sm:text-2xl">
           Built for Everyone. Powered by Passion.
         </p>
-        <p className="text-sm text-center px-90 mt-7">
+        <p className="text-sm text-center px-90 mt-7 max-sm:px-2 ">
           We provide the best environment, the best equipment, and the best
           coaching to help
           <br /> people achieve their absolute peak potential.
@@ -50,13 +50,15 @@ const AboutUsSection = () => {
           <br /> facility is designed to push you further.
         </p>
       </div>
-      <div className="flex items-center justify-center gap-48 my-7">
+      <div className=" max-sm:mb-0 max-sm:gap-y-5 flex items-center justify-center gap-48 my-7 max-sm:grid max-sm:gap-2 max-sm:grid-cols-2 max-sm:place-items-center">
         {aboutData.map((item, index) => (
           <div
             key={index}
-            className="rounded-full border-2 border-(--primary-color) w-27 h-27 flex flex-col items-center justify-center">
+            className="rounded-full border-2 border-(--primary-color) w-27 h-27 flex flex-col items-center justify-center
+            max-sm:w-24 max-sm:h-24 max-sm:rounded-full 
+            ">
             {item.icon}
-            <p className="text-center text-sm mt-3">{item.text1}<br/>{item.text2}</p>
+            <p className="text-center text-sm mt-3 max-sm:text-xs">{item.text1}<br/>{item.text2}</p>
           </div>
         ))}
       </div>
