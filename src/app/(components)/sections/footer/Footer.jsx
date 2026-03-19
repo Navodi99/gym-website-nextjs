39d239b34b04f43b1d20b8c163829357fc373d72
Navodi99 <navodil99@gmail.com>
2026-03-19 14:53:06 +0530

    add responsive for footer

--- src/app/(components)/sections/footer/Footer.jsx
+++ src/app/(components)/sections/footer/Footer.jsx
@@ -3,21 +3,21 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-between border-t border-(--primary-color) py-2">
+    <div className="flex items-center justify-between border-t border-(--primary-color) py-2 max-sm:gap-3 ">
       <div className="">
-        <div className="rounded-full w-20 overflow-hidden ">
-          <img src="/images/GymLogo.jpg" alt="Logo" className="w-40 " />
+        <div className="rounded-full w-20 overflow-hidden max-sm:w-10">
+          <img src="/images/GymLogo.jpg" alt="Logo" className="w-40 max-sm:w-20"  />
         </div>
       </div>
 
       <div>
-        <p className="text-sm">© 2026  FITNESS CENTER. All rights reserved.</p>
+        <p className="text-sm max-sm:text-xs ">© 2026  FITNESS CENTER. All rights reserved.</p>
       </div>
       <div>
         <div className="text-(--primary-color) flex items-center justify-center gap-7">
-          <Facebook className="w-8 h-8"/>
-          <Instagram className="w-8 h-8"/>
-          <Linkedin className="w-8 h-8"/>
+          <Facebook className="w-8 h-8 max-sm:w-4 max-sm:h-4"/>
+          <Instagram className="w-8 h-8 max-sm:w-4 max-sm:h-4"/>
+          <Linkedin className="w-8 h-8 max-sm:w-4 max-sm:h-4"/>
         </div>
       </div>
     </div>
