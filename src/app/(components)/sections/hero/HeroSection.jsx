43b98cc378fd9a0949e37a1b8ea82e316bbf2a15
Navodi99 <navodil99@gmail.com>
2026-03-19 14:23:12 +0530

    add responsive for hero

--- src/app/(components)/sections/hero/HeroSection.jsx
+++ src/app/(components)/sections/hero/HeroSection.jsx
@@ -3,21 +3,33 @@ import CustomButton from "../../reusables/buttons/CustomButton";
 
 const HeroSection = () => {
   return (
-    <div className="flex items-center justify-between p-5 my-15 " id="home">
+    <div className="flex items-center justify-between p-5 my-15 max-sm:flex-col max-sm:my-2
+     max-sm:bg-[url('/images/hero.png')]
+        max-sm:bg-size-[420px] 
+        max-sm:bg-center 
+        max-sm:bg-no-repeat 
+        max-sm:relative
+         max-sm:bg-(--background)/60
+  max-sm:bg-blend-darken
+        " 
+
+ 
+
+        id="home">
         <div className="pb-10 flex  flex-col">
-            <p className="uppercase tracking-widest font-semibold">Built Strong</p>
-            <p className="text-6xl mt-6">Transform<span className="text-(--primary-color)"> Your Body.</span> </p>
-            <p className="text-6xl mt-6">Transform<span className="text-(--primary-color)"> Your Life.</span></p>
+            <p className="uppercase tracking-widest font-semibold max-sm:text-center ">Built Strong</p>
+            <p className="text-6xl mt-6 max-sm:text-4xl max-sm:text-center">Transform<span className="text-(--primary-color)"> Your Body.</span> </p>
+            <p className="text-6xl mt-6 max-sm:text-4xl max-sm:text-center">Transform<span className="text-(--primary-color)"> Your Life.</span></p>
             <p className="font-light mt-6">
             Join the ultimate fitness experience with expert trainers, modern<br/>
             equipment, and personalized workout plans.
             </p>
-            <div className="flex gap-3 mt-6">
-                <CustomButton>Join Now</CustomButton>
-                <CustomButton invert>View Plans</CustomButton>
+            <div className="flex gap-3 mt-6 max-sm:flex-col max-sm:items-center">
+                <CustomButton >Join Now</CustomButton>
+                <CustomButton invert >View Plans</CustomButton>
             </div>
         </div>
-        <div>
+        <div className="max-sm:hidden">
             <img src="/images/hero.png" alt="" className="w-2xl filter brightness-70" />
         </div>
     </div>
