fffaf2d105ae0e5744abfa4d62a72b48351a8ce9
Navodi99 <navodil99@gmail.com>
2026-03-19 19:54:14 +0530

    add animation to about section

--- src/app/(components)/sections/about/AboutUsSection.jsx
+++ src/app/(components)/sections/about/AboutUsSection.jsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   AudioWaveform,
   BicepsFlexed,
@@ -5,6 +6,8 @@ import {
   HeartHandshake,
 } from "lucide-react";
 import React from "react";
+import { motion } from "framer-motion";
+import { container, fadeIn, fadeUp, imageAnim } from "@/app/(utils)/animation";
 
 const AboutUsSection = () => {
   const aboutData = [
@@ -31,15 +34,29 @@ const AboutUsSection = () => {
   ];
 
   return (
-    <div className="  max-sm:flex-col bg-(--gray-color) border-t-4 border-(--primary-color) rounded-3xl mb-15 p-5 max-sm:my-2" id="about">
-      <div>
-        <p className="uppercase tracking-widest font-bold text-2xl text-(--primary-color) text-center  max-sm:text-xl">
+    <motion.div
+      variants={container}
+      initial="hidden"
+      animate="show"
+      className="  max-sm:flex-col bg-(--gray-color) border-t-4 border-(--primary-color) rounded-3xl mb-15 p-5 max-sm:my-2"
+      id="about"
+    >
+      <motion.div
+      variants={container}
+      >
+        <motion.p
+        variants={fadeUp}
+        className="uppercase tracking-widest font-bold text-2xl text-(--primary-color) text-center  max-sm:text-xl">
           About Us
-        </p>
-        <p className="text-4xl text-center mt-5 max-sm:text-2xl">
+        </motion.p>
+        <motion.p 
+        variants={fadeUp}
+        className="text-4xl text-center mt-5 max-sm:text-2xl">
           Built for Everyone. Powered by Passion.
-        </p>
-        <p className="text-sm text-center px-90 mt-7 max-sm:px-2 ">
+        </motion.p>
+        <motion.p
+        variants={fadeIn}
+         className="text-sm text-center px-90 mt-7 max-sm:px-2 ">
           We provide the best environment, the best equipment, and the best
           coaching to help
           <br /> people achieve their absolute peak potential.
@@ -48,21 +65,27 @@ const AboutUsSection = () => {
           <br /> results. Whether you're a seasoned athlete or just starting
           your fitness journey, our
           <br /> facility is designed to push you further.
-        </p>
-      </div>
-      <div className=" max-sm:mb-0 max-sm:gap-y-5 flex items-center justify-center gap-48 my-7 max-sm:grid max-sm:gap-2 max-sm:grid-cols-2 max-sm:place-items-center">
+        </motion.p>
+      </motion.div>
+      <motion.div
+      variants={container}
+      className=" max-sm:mb-0 max-sm:gap-y-5 flex items-center justify-center gap-48 my-7 max-sm:grid max-sm:gap-2 max-sm:grid-cols-2 max-sm:place-items-center">
         {aboutData.map((item, index) => (
-          <div
+          <motion.div
+          variants={imageAnim}
             key={index}
-            className="rounded-full border-2 border-(--primary-color) w-27 h-27 flex flex-col items-center justify-center
-            max-sm:w-24 max-sm:h-24 max-sm:rounded-full 
-            ">
+            className="rounded-full border-2 border-(--primary-color) w-27 h-27 flex flex-col items-center justify-center max-sm:w-24 max-sm:h-24 max-sm:rounded-full "
+          >
             {item.icon}
-            <p className="text-center text-sm mt-3 max-sm:text-xs">{item.text1}<br/>{item.text2}</p>
-          </div>
+            <p className="text-center text-sm mt-3 max-sm:text-xs">
+              {item.text1}
+              <br />
+              {item.text2}
+            </p>
+          </motion.div>
         ))}
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
--- src/app/(components)/sections/hero/HeroSection.jsx
+++ src/app/(components)/sections/hero/HeroSection.jsx
@@ -3,60 +3,27 @@ import React from "react";
 import { motion } from "framer-motion";
 import CustomButton from "../../reusables/buttons/CustomButton";
 import clsx from "clsx";
+import { container, fadeUp, imageAnim } from "@/app/(utils)/animation";
 
 const HeroSection = () => {
-  
-  const container = {
-    hidden: {},
-    show: {
-      transition: {
-        staggerChildren: 0.2,
-      },
-    },
-  };
-
-  
-  const fadeUp = {
-    hidden: { opacity: 0, y: 40 },
-    show: {
-      opacity: 1,
-      y: 0,
-      transition: { duration: 0.6, ease: "easeOut" },
-    },
-  };
-
-  
-  const imageAnim = {
-    hidden: { opacity: 0, scale: 0.9 },
-    show: {
-      opacity: 1,
-      scale: 1,
-      transition: { duration: 0.8, ease: "easeOut" },
-    },
-  };
-
   return (
-<motion.div
-  variants={container}
-  initial="hidden"
-  animate="show"
-  id="home"
-  className={clsx(
-    "flex items-center justify-between p-5 my-15",
-    "max-sm:flex-col max-sm:my-2",
-    "max-sm:bg-[url('/images/hero.png')]",
-    "max-sm:bg-size-[420px]",
-    "max-sm:bg-center max-sm:bg-no-repeat",
-    "max-sm:relative",
-    "max-sm:bg-(--background)/60",
-    "max-sm:bg-blend-darken"
-  )}
->
-     
-      <motion.div
-        className="pb-10 flex flex-col"
-        variants={container}
-      >
+    <motion.div
+      variants={container}
+      initial="hidden"
+      animate="show"
+      id="home"
+      className={clsx(
+        "flex items-center justify-between p-5 my-15",
+        "max-sm:flex-col max-sm:my-2",
+        "max-sm:bg-[url('/images/hero.png')]",
+        "max-sm:bg-size-[420px]",
+        "max-sm:bg-center max-sm:bg-no-repeat",
+        "max-sm:relative",
+        "max-sm:bg-(--background)/60",
+        "max-sm:bg-blend-darken",
+      )}
+    >
+      <motion.div className="pb-10 flex flex-col" variants={container}>
         <motion.p
           variants={fadeUp}
           className="uppercase tracking-widest font-semibold max-sm:text-center"
@@ -95,11 +62,7 @@ const HeroSection = () => {
         </motion.div>
       </motion.div>
 
-      
-      <motion.div
-        variants={imageAnim}
-        className="max-sm:hidden"
-      >
+      <motion.div variants={imageAnim} className="max-sm:hidden">
         <img
           src="/images/hero.png"
           alt=""
